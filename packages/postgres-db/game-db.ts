fd41d1294a8887a0dec84265a9e103be912c4d27
Matthew Gohr <Gohr89@gmail.com>
2022-10-03 19:31:03 -0400

    setup game db module

--- packages/postgres-db/game-db.ts
+++ packages/postgres-db/game-db.ts
@@ -1,19 +1,43 @@
 import { Pool } from 'pg';
 
+import { Game, IGame } from '../models/game';
 import { DB } from './db';
 
 export class GameDb {
   private pool: Pool;
-
+  private schemaColumnStr =
+    'g.*, p.age as player_age, p.name as player_name, t.name as team_name, p.position as player_position, p.number as player_number';
   /**
    * getGameData
    */
-  public async getGameData(): Promise<any> {
+  public async getGames(): Promise<any> {
     if (!this.pool) {
       this.pool = await DB.getPool();
     }
-    await this.pool.query({
-      text: 'SELECT * FROM game',
+    const result = await this.pool.query<IGame>({
+      text:
+        `SELECT ${this.schemaColumnStr} FROM game g ` +
+        'LEFT JOIN team t on t."id"=g.team_id ' +
+        'LEFT JOIN player p on p."id"=player_id',
     });
+    return result.rows.map((g) => new Game(g));
+  }
+  public async getAllTodaysGamesByExternalIds(
+    externalIds: number[]
+  ): Promise<any> {
+    if (!this.pool) {
+      this.pool = await DB.getPool();
+    }
+    const result = await this.pool.query<IGame>({
+      text:
+        `SELECT ${this.schemaColumnStr} FROM game g ` +
+        'LEFT JOIN team t on t."id"=g.team_id ' +
+        'LEFT JOIN player p on p."id"=player_id ' +
+        'WHERE g.game_identifier = ANY($1) ' +
+        'and g.is_final is false ' +
+        'Date(g.start_date) = CURRENT_DATE',
+      values: [externalIds],
+    });
+    return result.rows.map((g) => new Game(g));
   }
 }
