24e7a6c49a39dc77b24e01d9697ea55f01c11945
Matthew Gohr <Gohr89@gmail.com>
2022-10-03 20:47:11 -0400

    update aws util

--- packages/aws/aws-util.ts
+++ packages/aws/aws-util.ts
@@ -1,15 +1,26 @@
 import * as AWS from 'aws-sdk';
 
+import { GameWatcherSNSEvent } from '../models/interfaces';
+
 export class AwsUtil {
-  public static async sendSns(message: string): Promise<any> {
-    const sns = new AWS.SNS({
-      region: process.env['REGION'],
-    });
-    return sns
-      .publish({
-        Message: message,
-        TopicArn: process.env['GAME_WATCHER_SNS'],
-      })
-      .promise();
+  public static async sendWatcherSns(data: GameWatcherSNSEvent): Promise<void> {
+    return this.sendSns(JSON.stringify(data), process.env['GAME_WATCHER_SNS']);
+  }
+  private static async sendSns(message: string, topic: string): Promise<any> {
+    if (process.env['STAGE'] === 'local') {
+      console.log('========START SNS MESSAGE=========');
+      console.log(message);
+      console.log('========END SNS MESSAGE=========');
+    } else {
+      const sns = new AWS.SNS({
+        region: process.env['REGION'],
+      });
+      return sns
+        .publish({
+          Message: message,
+          TopicArn: topic,
+        })
+        .promise();
+    }
   }
 }
---
+++ packages/models/interfaces.ts
@@ -0,0 +1,4 @@
+export interface GameWatcherSNSEvent {
+  gameId?: number;
+  gameUrl: string;
+}
