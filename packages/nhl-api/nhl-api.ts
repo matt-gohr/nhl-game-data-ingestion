b491e517899fd487af7d44d28be67d3e8e64ad28
Matthew Gohr <Gohr89@gmail.com>
2022-10-04 00:05:20 -0400

    get game data

--- packages/nhl-api/nhl-api.ts
+++ packages/nhl-api/nhl-api.ts
@@ -1,10 +1,11 @@
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 
-import { ScheduledResponse } from '../models/api-responses/scheduled';
+import { GameResponse, ScheduledResponse } from '../models/api-responses/scheduled';
 
 export class NhlApi {
-  public async getTodaysSchedule(): Promise<ScheduledResponse> {
-    const url = 'https://statsapi.web.nhl.com/api/v1/schedule';
+  private static baseUrl = 'https://statsapi.web.nhl.com';
+  public static async getTodaysSchedule(): Promise<ScheduledResponse> {
+    const url = this.baseUrl + '/api/v1/schedule';
     const startDate = new Date();
     const endDate = new Date();
     startDate.setDate(startDate.getDate() - 1);
@@ -15,11 +16,11 @@ export class NhlApi {
       },
     };
     const r: AxiosResponse<ScheduledResponse> = await axios.get(url, config);
-    if (r.data && r.data.dates.length > 0) {
-      return r.data;
-    } else {
-      // TODO: send to some error log service
-      throw new Error('no data returned from api');
-    }
+    return r.data;
+  }
+  public static async getGameData(path: string): Promise<GameResponse> {
+    const url = this.baseUrl + path;
+    const r: AxiosResponse<GameResponse> = await axios.get(url);
+    return r.data;
   }
 }
